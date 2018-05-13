cbdc1d8acb17f7c047ddb55a2b8d6f0ab1e9ea9a
Dominic Spreitz <dominic@spreitz.de>
2018-05-13 17:18:23 +0200

    Introduce python version specific return

--- flarmnet/encryption.py
+++ flarmnet/encryption.py
@@ -1,4 +1,5 @@
 def decrypt(input):
+    import sys
     characters = []
     for i in range(0, len(input), 2):
         ascii = int(input[i:i + 2], 16)
@@ -8,4 +9,9 @@ def decrypt(input):
     if len(output) == 0:
         return None
 
-    return output
+    if sys.version_info[0] >= 3:
+        # Python 3 specific definitions
+        return output
+    else:
+        # Python 2 specific definitions
+        return.decode('iso-8859-1')
